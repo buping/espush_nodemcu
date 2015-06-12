769f212f3fcf08c33e36aadf74a67375701421ec
sunchengcheng <sun@chengcheng.info>
2015-06-12 10:47:23 +0800

    add const var.

--- app/modules/push.c
+++ app/modules/push.c
@@ -15,11 +15,16 @@
  *
  */
 
+
 static int push_data_recved = LUA_NOREF;
 static lua_State* gL = NULL;
 
 void msg_recv(uint8* pdata, uint32 len)
 {
+	if(push_data_recved == LUA_NOREF) {
+		return;
+	}
+
 	lua_rawgeti(gL, LUA_REGISTRYINDEX, push_data_recved);
 
 	lua_pushlstring(gL, (const char*)pdata, len);
@@ -31,6 +36,13 @@ static int regist(lua_State* L)
 {
 	uint32 appid = luaL_checkinteger(L, 1);
 	const char* appkey = luaL_checkstring(L, 2);
+	if(!appkey || os_strlen(appkey) != 32) {
+		return luaL_error(L, "appkey arguments error");
+	}
+
+	if(push_server_connect_status() == STATUS_CONNECTED) {
+		return luaL_error(L, "connected.");
+	}
 
 	gL = L;
 	if (lua_type(L, 3) == LUA_TFUNCTION || lua_type(L, 3) == LUA_TLIGHTFUNCTION) {
@@ -44,7 +56,8 @@ static int regist(lua_State* L)
 
 	push_register(appid, (char*)appkey, msg_recv);
 
-	return 0;
+	lua_pushinteger(L, 0);
+	return 1;
 }
 
 
@@ -82,7 +95,13 @@ const LUA_REG_TYPE push_map[] = {
 	{LSTRKEY("unregist"), LFUNCVAL(unregist)},
 	{LSTRKEY("get_status"), LFUNCVAL(get_status)},
 	{LSTRKEY("pushmsg"), LFUNCVAL(pushmsg)},
-	{LSTRKEY("__metatable"), LROVAL(push_map) },
+
+	{LSTRKEY("CONNECTING"), LNUMVAL(STATUS_CONNECTING)},
+	{LSTRKEY("DNS_LOOKUP"), LNUMVAL(STATUS_DNS_LOOKUP)},
+	{LSTRKEY("CONNECTED"), LNUMVAL(STATUS_CONNECTED)},
+	{LSTRKEY("DISCONNECTED"), LNUMVAL(STATUS_DISCONNECTED)},
+
+	{LSTRKEY("__metatable"), LROVAL(push_map)},
 	{ LNILKEY, LNILVAL }
 };
 
@@ -94,6 +113,6 @@ LUALIB_API int luaopen_push( lua_State *L )
 
 /*
  * TODO:
- * [ ] 加入常量
- * [ ] 加入错误判断，加入重复regist的判断
+ * [√] 加入常量
+ * [√] 加入错误判断，加入重复regist的判断
  */
